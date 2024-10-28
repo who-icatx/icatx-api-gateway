56573af95276b0f6dabd2b324ca174868bdeacd0
alexsilaghi <silaghialexmihail@gmail.com>
2024-10-28 16:47:43 +0200

    fixed rabbitmq connection

--- src/main/java/edu/stanford/protege/gateway/IcatxApiGatewayApplication.java
+++ src/main/java/edu/stanford/protege/gateway/IcatxApiGatewayApplication.java
@@ -1,16 +1,17 @@
 package edu.stanford.protege.gateway;
 
 import edu.stanford.protege.webprotege.ipc.WebProtegeIpcApplication;
+import edu.stanford.protege.webprotege.ipc.impl.RabbitMqConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
-@Import(WebProtegeIpcApplication.class)
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
+@SpringBootApplication
+@Import({WebProtegeIpcApplication.class})
 public class IcatxApiGatewayApplication {
 
 	public static void main(String[] args) {
