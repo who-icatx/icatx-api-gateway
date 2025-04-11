539a77b87913b50e582e6512b5442c6335c2a48e
alexsilaghi <silaghialexmihail@gmail.com>
2025-04-11 14:07:25 +0300

    logging test 3.

--- src/main/java/edu/stanford/protege/gateway/OwlEntityService.java
+++ src/main/java/edu/stanford/protege/gateway/OwlEntityService.java
@@ -131,7 +131,7 @@ public class OwlEntityService {
     }
 
     public Set<ProjectSummaryDto> getProjects() {
-        LOGGER.info("ALEX deployed in get projects 2");
+        LOGGER.info("ALEX deployed in get projects 3");
         CompletableFuture<Set<ProjectSummaryDto>> availableProjects = ontologyService.getProjects();
         try {
             return availableProjects.get();
