1f6dca24d681f12a03026b7361c91460143ee838
soimugeo <soimugeo@gmail.com>
2025-04-25 12:09:15 +0300

    [https://github.com/who-icatx/icatx-project/issues/191]: change field order in OWLEntityDto

--- src/main/java/edu/stanford/protege/gateway/dto/OWLEntityDto.java
+++ src/main/java/edu/stanford/protege/gateway/dto/OWLEntityDto.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@JsonPropertyOrder({ "entityUri", "languageTerms", "entityLinearizations", "postcoordination", "logicalConditions", "parents"})
+@JsonPropertyOrder({ "entityUri", "isObsolete", "languageTerms", "entityLinearizations", "postcoordination", "logicalConditions", "parents"})
 public record OWLEntityDto(@JsonProperty("entityUri") String entityIRI,
 
                            boolean isObsolete,
