4b04859b647df3020b30c67112f8c99f9f264091
alexandru.silaghi <alexandru.silaghi@whitebox.tech>
2026-02-03 09:21:07 +0200

    trying to force https on server

--- src/main/java/edu/stanford/protege/gateway/config/OpenApiConfiguration.java
+++ src/main/java/edu/stanford/protege/gateway/config/OpenApiConfiguration.java
@@ -2,20 +2,28 @@ package edu.stanford.protege.gateway.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.SpecVersion;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfiguration {
 
+    @Value("${server.servlet.context-path:/icatx/api/}")
+    private String contextPath;
+
+    @Value("${springdoc.swagger-ui.server-url:}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+        OpenAPI openAPI = new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().
                         addList("Bearer Authentication"))
                 .components(new Components().addSecuritySchemes
                         ("Bearer Authentication", createAPIKeyScheme()))
@@ -23,6 +31,12 @@ public class OpenApiConfiguration {
                         .description("API exposed by iCAT-X application")
                         .version("1.0")
                         .contact(new Contact().name("Alex Silaghi")));
+
+        if (serverUrl != null && !serverUrl.isEmpty()) {
+            openAPI.setServers(null);
+            openAPI.addServersItem(new Server().url(serverUrl).description("Production server"));
+        }
+        return openAPI;
     }
 
     private SecurityScheme createAPIKeyScheme() {
